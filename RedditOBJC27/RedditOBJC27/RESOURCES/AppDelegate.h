2a91e0c80a6d70bba17f98533b6ff23f9b991cb8
Austin West <korruptedanomaly@gmail.com>
2019-07-03 10:51:53 -0600

    File hierarchy set up and ready for Dev

---
+++ RedditOBJC27/RedditOBJC27/RESOURCES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditOBJC27
+//
+//  Created by Austin West on 7/3/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
