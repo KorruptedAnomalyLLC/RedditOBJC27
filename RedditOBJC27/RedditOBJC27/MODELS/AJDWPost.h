572210b5a92a9a31d9831c0048c0749f509e7db3
Austin West <korruptedanomaly@gmail.com>
2019-07-03 11:05:46 -0600

    Storyboard set up

---
+++ RedditOBJC27/RedditOBJC27/MODELS/AJDWPost.h
@@ -0,0 +1,17 @@
+//
+//  AJDWPost.h
+//  RedditOBJC27
+//
+//  Created by Austin West on 7/3/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AJDWPost : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RedditOBJC27/RedditOBJC27/MODELS/AJDWPost.m
@@ -0,0 +1,13 @@
+//
+//  AJDWPost.m
+//  RedditOBJC27
+//
+//  Created by Austin West on 7/3/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import "AJDWPost.h"
+
+@implementation AJDWPost
+
+@end
